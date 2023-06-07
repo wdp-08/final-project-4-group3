683bd078c9e590bd54561344fce043ec19890c1f
Triyonorifan11 <triyonorifan01@gmail.com>
2023-06-07 15:19:49 +0700

    create login register view dom

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Detail from '../views/pages/detail';
 import Login from '../views/pages/login';
+import Register from '../views/pages/register';
 
 const routes = {
   '/': Login, // default page
+  '/register': Register,
   '/detail': Detail,
 };
 
--- src/scripts/views/pages/register.js
+++ src/scripts/views/pages/register.js
@@ -1,4 +1,4 @@
-const Login = {
+const Register = {
   async render() {
     return `
     <div style="background-image: url('./images/cover.jpg'); width: 100%; height: 100vh;">
@@ -6,13 +6,13 @@ const Login = {
             <div class="d-flex justify-content-center flex-wrap" style="height: 100%;  width: 100%; align-items: center;">
                 <div class="col-md-6 col-12 " style="padding-top: 8rem;">
                     <div class="card text-center py-5">
-                        <h2 class="mb-3 fw-bold text-primary-me">Login</h2>
+                        <h2 class="mb-3 fw-bold text-primary-me">Register</h2>
                         <div style="border-bottom: 0.1rem solid #00A496;  width: 3.5rem; margin: auto; "></div>
                         <button type="submit" class="btn btn-light border-1 my-5">
                             <span class="google-icon"><img src="./images/logo-google.png" alt="Google"></span>
-                            Login with Google</button>
-                        <p class="mb-2">Don't have an account? 
-                            <a href="#/register">Register</a>
+                            Register with Google</button>
+                        <p class="mb-2">Do you have an account? 
+                            <a href="#/">Login!</a>
                         </p>
 
                         <div style="border-bottom: 2px solid #000000;  width: 80%; margin: 10px auto; "></div>
@@ -30,4 +30,4 @@ const Login = {
   },
 };
 
-export default Login;
+export default Register;
