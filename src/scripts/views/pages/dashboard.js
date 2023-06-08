2414f40027dbc6a8f782f8b6cf96cb4e8805b547
Triyonorifan11 <triyonorifan01@gmail.com>
2023-06-08 09:42:46 +0700

    update view dashboard

--- src/scripts/views/pages/dashboard.js
+++ src/scripts/views/pages/dashboard.js
@@ -1,97 +1,129 @@
+import Swal from 'sweetalert2';
 import { getUserInfo, innerElement, redirect } from '../../utils/functions';
 
 const Dashboard = {
   async render() {
     return `
-    <main class="pb-5" style="background-color: #00B6A6;">
+    <section class="pb-5" style="background-color: #00B6A6; min-height:100vh;">
         <div class="container">
-        <div class="d-flex justify-content-end">
-            <button class="btn btn-light mt-4">logout</button>
-        </div>
-        <div class="card mt-5 mb-5">
-            <div class="card-body">
-                <div class="row">
-                <!-- col 1 start -->
-                <div class="col-lg-5 text-center">
-                    <div class="card mt-2 ms-3" style="box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);">
-                        <div class="card-body">
-                            <img src="./images/profile.png" class="card-img-top" alt="profile" style="max-width: 50%; height: auto" id="url_foto" />
-                            <h5 class="card-title" id="nama_user">Admin</h5>
-                            <p class="card-text" id="email">admin@gmail.com</p>
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-light mt-4 text-primary-me fw-bold" id="btn-logout"><i class="bi bi-box-arrow-right me-2"></i> logout</button>
+            </div>
+            <div class="card mt-5 mb-5">
+                <div class="card-body p-5">
+                    <div class="row">
+
+
+                        <div class="col-lg-5">
+                            <div class="card mt-2 shadow-me border border-0 mb-4">
+                                <div class="card-body text-center">
+                                    <img src="./images/profile.png" class="card-img-top rounded rounded-circle" alt="profile" style="max-width: 50%; height: auto" id="url_foto" />
+                                    <h5 class="card-title text-capitalize my-3" id="nama_user">Admin</h5>
+                                    <p class="card-text" id="email">admin@gmail.com</p>
+                                </div>
+                            </div>
+                            <div class="card mt-2 border border-0">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-slate-green fw-bold" type="button">Start QuizzMee</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-7">
+                            <div class="card mt-2 text-center shadow-me bg-green-me border border-0">
+                                <h1 style="color: #ffff;">Riwayat Quizz</h1>
+                            </div>
+                            <div id="list-history-score">
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-2 shadow-me border border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center px-4">
+                                            <span class="fs-5">22 Mei 2023</span>
+                                            <span class="fw-bold fs-3 text-center">90</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="card mt-2 ms-3">
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary" style="background-color: #00B6A6; color: #ffff;" type="button">Button</button>
-                        </div>
-                </div>
-
-                </div>
-                <!-- col 1 end -->
-                <!-- col 2 start -->
-                <div class="col-lg-7">
-                    <div class="card mt-2 text-center" style="background-color: #00B6A6; box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);" >
-                    <h1 style="color: #ffff;">Riwayat Quizz</h1>
-                </div>
-                <div class="card mt-2" style="box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);" >
-                    <div class="container">
-                        <div class="row align-items-start">
-                            <div class="col">
-                            <p class="mt-2">22 Mei 2023</p>
-                            </div>
-                            <div class="col">
-                            <h3 style="font-weight: bold;" class="mt-2 text-center">90</h3>
-                            </div>
-                        </div>
-                        </div>
-                </div>
-                <div class="card mt-2" style="box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);" >
-                    <div class="container">
-                        <div class="row align-items-start">
-                            <div class="col">
-                            <p class="mt-2">22 Mei 2023</p>
-                            </div>
-                            <div class="col">
-                            <h3 style="font-weight: bold;" class="mt-2 text-center">90</h3>
-                            </div>
-                        </div>
-                        </div>
-                </div>
-                <div class="card mt-2" style="box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);" >
-                    <div class="container">
-                        <div class="row align-items-start">
-                            <div class="col">
-                            <p class="mt-2">22 Mei 2023</p>
-                            </div>
-                            <div class="col">
-                            <h3 style="font-weight: bold;" class="mt-2 text-center">90</h3>
-                            </div>
-                        </div>
-                        </div>
-                </div>
-                <div class="card mt-2" style="box-shadow: 0 3px 5px rgb(0 0 0 / 0.2);" >
-                    <div class="container">
-                        <div class="row align-items-start">
-                            <div class="col">
-                            <p class="mt-2">22 Mei 2023</p>
-                            </div>
-                            <div class="col">
-                            <h3 style="font-weight: bold;" class="mt-2 text-center">90</h3>
-                            </div>
-                        </div>
-                        </div>
-                </div>
-                </div>
-                
-                <!-- col 2 end -->
                 </div>
             </div>
-            </div>
-
         </div>
-        </div>
-    </main>
+    </section>
     `;
+  },
+
+  async _logout() {
+    Swal.fire({
+      title: 'Do you want to Logout?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Logout',
+      confirmButtonColor: '#00B6A6',
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        localStorage.removeItem('token_access');
+        localStorage.removeItem('user_quizmee');
+        window.location.reload();
+      }
+    });
   },
 
   async afterRender() {
@@ -103,6 +135,9 @@ const Dashboard = {
       urlFoto.setAttribute('src', userAccess.url_foto);
       innerElement('#nama_user', userAccess.nama_user);
       innerElement('#email', userAccess.email);
+
+      const btnLogout = document.querySelector('#btn-logout');
+      btnLogout.addEventListener('click', this._logout);
     }
   },
 };
--- src/scripts/views/pages/register.js
+++ src/scripts/views/pages/register.js
@@ -8,17 +8,22 @@ const Register = {
             <div class="d-flex justify-content-center flex-wrap" style="height: 100%;  width: 100%; align-items: center;">
                 <div class="col-md-6 col-12 " style="padding-top: 8rem;">
                     <div class="card text-center py-5">
-                        <h2 class="mb-3 fw-bold text-primary-me">Register</h2>
-                        <div style="border-bottom: 0.1rem solid #00A496;  width: 3.5rem; margin: auto; "></div>
-                        <button type="submit" class="btn btn-light border-1 my-5" id="btn-register">
-                            <span class="google-icon"><img src="./images/logo-google.png" alt="Google"></span>
-                            Register with Google</button>
-                        <p class="mb-2">Do you have an account? 
-                            <a href="#/">Login!</a>
-                        </p>
+                        <div class="card-body d-grid justify-content-center">
+                            <h2 class="mb-3 fw-bold text-primary-me">Register</h2>
+                            <div style="border-bottom: 2px solid #00A496;  width: 100px; margin: auto; "></div>
+                            <button type="submit" class="btn btn-light border-me my-5 d-flex align-items-center" id="btn-register">
+                                <i class="google-icon me-3">
+                                <img src="./images/logo-google.png" alt="Google" width="50px">
+                                </i>
+                                <span>Register with Google</span>
+                            </button>
+                            <p class="mb-2">Do you have an account? 
+                                <a href="#/">Login</a>
+                            </p>
 
-                        <div style="border-bottom: 2px solid #000000;  width: 80%; margin: 10px auto; "></div>
-                        <h2 class="fw-bold text-primary-me">QuizzMee</h2>
+                            <div style="border-bottom: 2px solid #000000;  width: 80%; margin: 10px auto; "></div>
+                            <h2 class="fw-bold text-primary-me">QuizzMee</h2>
+                        </div>
                     </div>
                 </div>
             </div>
