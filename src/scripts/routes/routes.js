2350ece26383aa39c61c1c7f5375c1cc70bb5e5b
Dodistywn <dodisetya000@gmail.com>
2023-06-08 13:57:53 +0700

    add view score in src

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import Detail from '../views/pages/detail';
 import Login from '../views/pages/login';
 import Register from '../views/pages/register';
 import Dashboard from '../views/pages/dashboard';
+import Score from '../views/pages/score';
 
 const routes = {
   '/': Login, // default page
   '/register': Register,
   '/detail': Detail,
   '/dashboard': Dashboard,
+  '/score': Score,
 };
 
 export default routes;
